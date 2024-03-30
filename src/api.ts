1baaa88db5e57db220c4389f81e090459a4006eb
Eduardo <edualves15@gmail.com>
2024-03-29 21:18:48 -0300

    Atualização da URL do back-end

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 // Configuração da URL base para todas as requisições
-axios.defaults.baseURL = 'http://localhost:3030';
+// axios.defaults.baseURL = 'http://localhost:3030';
+axios.defaults.baseURL = 'https://hidrolab-backend-6ssfthxdpa-rj.a.run.app';
 
 // Envio de cookies com cada requisição
 axios.defaults.withCredentials = true;
